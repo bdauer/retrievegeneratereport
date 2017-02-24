e91338269314f2d8074dae43abd29364c1c7018e
bdauer <dauer.benjamin@gmail.com>
2017-02-23 23:36:37 -0500

    Remove unused/unfinished method

--- timer.py
+++ timer.py
@@ -13,16 +13,3 @@ def timethis(func):
         print("{} data retrieval time: {}".format(result["site_name"], total_time))
         return result
     return wrapper
-
-
-def timeprogram(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
-        total_time = '{}'.format(end - start)
-        result["time_to_complete"] = total_time
-        print("{} data retrieval time: {}".format(result["site_name"], total_time))
-        return result
-    return wrapper
