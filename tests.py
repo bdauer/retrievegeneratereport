6993791c016c4a433355ad54d99e4670a9f839f8
bdauer <dauer.benjamin@gmail.com>
2017-02-24 07:57:30 -0500

    Added tests to cover the major players in siteretriever.py

--- tests.py
+++ tests.py
@@ -1,6 +1,10 @@
+import unittest
 from reportbuilder import ReportBuilder, WordCountReportBuilder
 from siteretriever import ListingsRetriever, SiteRetriever
-import unittest
+from test_data import alexa_text, alexa_listings
+import requests
+import requests_mock
+from bs4 import BeautifulSoup
 
 
 class BuilderTestCase(unittest.TestCase):
@@ -52,17 +56,55 @@ class BuilderTestCase(unittest.TestCase):
 class ListingsRetrieverTestCase(unittest.TestCase):
 
     def setUp(self):
-        l = ListingsRetriever("me@me.com", "secretpassword")
+        self.lr = ListingsRetriever("me@me.com", "secret")
 
-        # test get_listings with a mock site
+        soup = BeautifulSoup(alexa_text, 'html.parser')
+        listings_table = soup.find("div", "listings table")
+        page_listings = listings_table.find_all("div", "site-listing")
+        self.soupy_listings = list(page_listings)
 
+    def test_scrub_listings(self):
+        scrubbed_listings = self.lr._scrub_listings(self.soupy_listings)
+        self.assertEqual(scrubbed_listings, alexa_listings)
 
 class SiteRetrieverTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.sr = SiteRetriever()
 
-    # test build sites list with a mock site.
+        adapter = requests_mock.Adapter()
+        adapter.register_uri('GET',
+                             'mock://google.com',
+                             headers={"headerkey":"someval"},
+                             cookies={"choc_chip":"ok"},
+                             text="two words")
+        session = requests.Session()
+        session.mount('mock', adapter)
+        self.resp = session.get("mock://google.com")
+
+    def test_get_data_from(self):
+        headers, cookies, word_count = self.sr._get_data_from(self.resp)
+        self.assertEqual(headers, ["headerkey"])
+        self.assertEqual(cookies, ["choc_chip"])
+        self.assertEqual(word_count, 2)
+
+    def test_build_site_dictionary(self):
+        generated_dict = self.sr._build_site_dictionary(self.resp, "google.com")
+        # using assertIn instead of assertEqual
+        # because the timethis decorator adds a dynamic value to the dictionary.
+        # could have also deleted or overwritten 'time_to_complete'
+        self.assertIn(("site_name", 'google.com'), generated_dict.items())
+        self.assertIn(("headers", ['headerkey']), generated_dict.items())
+        self.assertIn(("cookies", ['choc_chip']), generated_dict.items())
+        self.assertIn(("word_count", 2), generated_dict.items())
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
