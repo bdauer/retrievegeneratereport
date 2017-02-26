b586cbcbfbc0980143ec5d4905a3bd67dbe80e6a
bdauer <dauer.benjamin@gmail.com>
2017-02-26 17:40:27 -0500

    New NonStandardRowMixin (see the docstring)! Moved nonstandard row functionality out of the standard report builder. Updated the old boilerplate subclasses to use the new approach, but ideally should instantiate the new CustomRowReportBuilder class, which implements the mixin with the standard report, directly.

--- reportbuilder.py
+++ reportbuilder.py
@@ -4,7 +4,6 @@ class BaseReportBuilder(metaclass=ABCMeta):
     """
     Base class for report builders.
     """
-
     def __init__(self, header, categories, data):
         self.data = data
         self.categories = categories
@@ -17,33 +16,125 @@ class BaseReportBuilder(metaclass=ABCMeta):
     def build_report(self):
         pass
 
+class NonStandardRowMixin:
+    """
+    Used to add a nonstandard row to a report.
 
-# what changes:
-# categories, header, unique rows (linked to categories), report format (html, css etc)
-# what stays the same:
-# building and creating a report, building a table
+    Currently the only nonstandard functionality is to reduce a column,
+    specifically one that has been averaged or summed.
 
-# make categories an input to __init__
-# make unique rows some sort of mixin. It needs to override build_table
-# For each row, I need the type of calculation and the category name.
-# I'll also need to have different implementations for different report formats.
-# Maybe specify format as well, or store format as a ReportBuilder attribute
-# and grab that, so it's not user specified.
+    The NonStandardRowMixin has an additional required keyword argument:
+    _reduced_columns.
+    The value should be a list of dictionaries in the following format:
+    {"column_name": this should be the name of the column,
+      "method": sum or average,
+      "value": []}
 
-# class NonStandardRowMixin:
-#     """
-#     Used to add a nonstandard row to a report.
-#     """
-#     def build_report(self, file_format):
-#
-#         if file_format == "html":
-#             report = self.add_nonstandard_html_rows()
-#         return report
-#
-#     def _build_html_table(self):
-#
-#
-#     def _build_site_row(self):
+    Two cells will be appended to the table for each of the dictionaries.
+    The first cell describes gives the method and column name.
+    The second cell gives the new value.
+    """
+    def __init__(self, *args, _reduced_columns, **kwargs):
+        self._reduced_columns = _reduced_columns
+        super().__init__(*args, **kwargs)
+
+    def _update_list(self, current_category, value):
+        """
+        If the current category matches the sought_category parameter,
+        update the associated instance list attribute.
+
+        This is useful when values are being stored
+        for later processing/consumption.
+
+        current_category: the category being checked.
+        sought_category: the category that current_category must match.
+        list_to_update: the list to which a value will be added.
+        value: the value to be added.
+        """
+        for category_data in self._reduced_columns:
+            if current_category == category_data["column_name"]:
+                category_data["value"].append(float(value))
+
+    def _remove_closing_table_tag(self, table):
+        """
+        Return a table with the closing tag removed.
+
+        This is useful for subclasses
+        that add non-standard rows to the table.
+        """
+        return table[:-8]
+
+    def _build_reduce_row(self, method_applied, category_name, reduced_value):
+        """
+        Return a row formatted to display a single value.
+
+        Any time that a list is reduced,
+        this method can be used to generate a row for the value.
+        method_applied: the name of the method e.g. average or total.
+        category_name: the category of the data.
+        reduced_value: the value to be displayed.
+        """
+        reduced_row = """<tr>
+                        <td><b>{0} {1}:</b></td>
+                        <td><b>{2}</b></td>
+                        </tr>""".format(method_applied,
+                                        category_name,
+                                        reduced_value)
+        return reduced_row
+
+    def build_html_table(self):
+        """
+        Return a table of data.
+        This subclass adds a row for average word count.
+        """
+        table = super().build_html_table()
+        # I can get an add_row method from the mixin that does the following.
+        table = self._remove_closing_table_tag(table)
+
+        for special_column_data in self._reduced_columns:
+            for site_dict in self.data:
+                category = special_column_data["column_name"]
+                if category == site_dict["site_name"]:
+                    self._build_column_data(category, site_dict)
+
+            method = special_column_data["method"]
+            value = special_column_data["value"]
+            if method == "sum":
+                pretty_method = "total"
+                reduced_value = self._get_sum(value)
+            elif special_column_data["method"] == "average":
+                pretty_method = "avg"
+                reduced_value = self._get_average(value)
+            reduced_row = self._build_reduce_row(pretty_method, category,
+                                                       reduced_value)
+            table += reduced_row
+        table += "</table>"
+        return table
+
+    def _build_column_data(self, category, site_dict):
+        """
+        Return formatted data for a column.
+
+        category: a column name.
+        site_dict: a dict representing the data gathered from a site.
+        """
+        column_data = super()._build_column_data(
+                                category, site_dict)
+        category_value = site_dict[category]
+        self._update_list(category, category_value)
+        return column_data
+
+    def _get_average(self, some_list):
+        """
+        Return the average of all values in a list.
+        """
+        return sum(some_list) / len(some_list)
+
+    def _get_sum(self, some_list):
+        """
+        Return the sum of all values in a list.
+        """
+        return sum(some_list)
 
 class ReportBuilder(BaseReportBuilder):
     """
@@ -98,7 +189,6 @@ class ReportBuilder(BaseReportBuilder):
         table = self.build_html_table()
         report = "{0}{1}<br><br><br></body>".format(headings, table)
         return report
-
 
     def build_html_table(self):
         """
@@ -161,121 +251,24 @@ class ReportBuilder(BaseReportBuilder):
                         """.format(category_value)
         return category_html
 
-    def _get_average(self, some_list):
-        """
-        Return the average of all values in a list.
-        """
-        return sum(some_list) / len(some_list)
+class CustomRowReportBuilder(NonStandardRowMixin, ReportBuilder):
+    """
+    Use this class when adding a special row.
+    """
+    pass
 
-    def _get_sum(self, some_list):
-        """
-        Return the sum of all values in a list.
-        """
-        return sum(some_list)
-
-    def _update_list(self, current_category, sought_category,
-                          list_to_update, value):
-        """
-        If the current category matches the sought_category parameter,
-        update the associated instance list attribute.
-
-        This is useful when values are being stored
-        for later processing/consumption.
-
-        current_category: the category being checked.
-        sought_category: the category that current_category must match.
-        list_to_update: the list to which a value will be added.
-        value: the value to be added.
-        """
-        if current_category == sought_category:
-            list_to_update.append(float(value))
-
-    def _remove_closing_table_tag(self, table):
-        """
-        Return a table with the closing tag removed.
-
-        This is useful for subclasses
-        that add non-standard rows to the table.
-        """
-        return table[:-8]
-
-    def _build_reduce_row(self, method_applied, category_name, reduced_value):
-        """
-        Return a row formatted to display a single value.
-
-        Any time that a list is reduced,
-        this method can be used to generate a row for the value.
-        method_applied: the name of the method e.g. average or total.
-        category_name: the category of the data.
-        reduced_value: the value to be displayed.
-        """
-        reduced_row = """<tr>
-                        <td><b>{0} {1}:</b></td>
-                        <td><b>{2}</b></td>
-                        </tr>""".format(method_applied,
-                                        category_name,
-                                        reduced_value)
-        return reduced_row
-
-
-class WordCountReportBuilder(ReportBuilder):
+class WordCountReportBuilder(CustomRowReportBuilder):
     """
     Used to build a Word Count report.
     """
     def __init__(self, data):
         self.categories = ["site_name", "word_count"]
-        self.word_counts = []
+        reduced_columns = [{"column_name": "word_count",
+                           "method": "average",
+                           "value": []}]
         self.header = "Word Count Report"
-        super().__init__(self.header, self.categories, data)
-
-
-    def build_html_table(self):
-        """
-        Return a table of data.
-        This subclass adds a row for average word count.
-        """
-        table = super().build_html_table()
-        table = self._remove_closing_table_tag(table)
-        average_word_count = self._get_average(self.word_counts)
-        average_word_count_row = self._build_reduce_row("average", "word count",
-                                                         average_word_count)
-        table += average_word_count_row
-        table += "</table>"
-        return table
-
-    # def _build_site_row(self, site_dict):
-    #     """
-    #     Return a row containing data for all of the predefined categories.
-    #     """
-    #     site_row = ""
-    #     for category in self.categories:
-    #         category_value = site_dict[category]
-    #
-    #         if isinstance(category_value, list):
-    #             category_value = ", ".join(sorted(category_value))
-    #
-    #         category_html = """
-    #                         <td>{0}</td>
-    #                         """.format(category_value)
-    #         site_row += category_html
-    #         self._update_list(category, "word_count",
-    #                        self.word_counts, category_value)
-    #     return site_row
-
-
-    def _build_column_data(self, category, site_dict):
-        """
-        Return formatted data for a column.
-
-        category: a column data category.
-        site_dict: a dict representing the data gathered from a site.
-        """
-        column_data = super()._build_column_data(category, site_dict)
-        category_value = site_dict[category]
-        self._update_list(category, "word_count",
-                          self.word_counts, category_value)
-        return column_data
-
+        super().__init__(
+            self.header, self.categories, data, _reduced_columns=reduced_columns)
 
 class HeaderReportBuilder(ReportBuilder):
     """
@@ -287,40 +280,15 @@ class HeaderReportBuilder(ReportBuilder):
         super().__init__(self.header, self.categories, data)
 
 
-class PerformanceReportBuilder(ReportBuilder):
+class PerformanceReportBuilder(CustomRowReportBuilder):
     """
     Used to build a Performance report.
     """
     def __init__(self, data):
         self.categories = ["site_name", "time_to_complete"]
         self.header = "Performance Report"
-        self.performance_counts = []
-        super().__init__(self.header, self.categories, data)
-
-    def build_html_table(self):
-        """
-        Return a table of data.
-        This subclass adds a row for average word count.
-        """
-        table = super().build_html_table()
-        # I can get an add_row method from the mixin that does the following.
-        table = self._remove_closing_table_tag(table)
-        total_time = self._get_sum(self.performance_counts)
-        total_performance_row = self._build_reduce_row("total", "time",
-                                                       total_time)
-        table += total_performance_row
-        table += "</table>"
-        return table
-
-    def _build_column_data(self, category, site_dict):
-        """
-        Return formatted data for a column.
-
-        category: a column data category.
-        site_dict: a dict representing the data gathered from a site.
-        """
-        column_data = super()._build_column_data(category, site_dict)
-        category_value = site_dict[category]
-        self._update_list(category, "time_to_complete",
-                          self.performance_counts, category_value)
-        return column_data
+        reduced_columns = [{"column_name": "word_count",
+                           "method": "sum",
+                           "value": []}]
+        super().__init__(
+            self.header, self.categories, data, _reduced_columns=reduced_columns)
