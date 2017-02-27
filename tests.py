38bf0323b71f5985c5f47191c77ca9a610c24165
bdauer <dauer.benjamin@gmail.com>
2017-02-26 19:19:26 -0500

    Correct tests to work with the new version

--- tests.py
+++ tests.py
@@ -19,7 +19,8 @@ class BuilderTestCase(unittest.TestCase):
                 "headers": ["a", "b", "c"],
                 "cookies": ["oreo", "choc_chip", "hermit"],
                 "word_count": 145}]
-        self.r = ReportBuilder(data)
+        categories = []
+        self.r = ReportBuilder("Report", categories, data)
 
         self.plain_table = """<table border="1">
                     <tr><th>Ranking</th></tr>
@@ -37,20 +38,20 @@ class BuilderTestCase(unittest.TestCase):
                     <tr><th>Ranking</th><th>site name</th><th>word count</th></tr>
                     <tr><td>1</td><td>apple</td><td>42</td></tr>
                     <tr><td>2</td><td>pear</td><td>145</td></tr>
-                    <tr><td><b>average word count:</b></td><td><b>93.5</b></td></tr>
+                    <tr><td><b>avg word count:</b></td><td><b>93.5</b></td></tr>
                     </table>""".replace("\n", "").replace(" ", "")
 
     def test_plain_table(self):
-        plain_table = self.r.build_table().replace("\n", "").replace(" ", "")
+        plain_table = self.r.build_html_table().replace("\n", "").replace(" ", "")
         self.assertEqual(plain_table, self.plain_table)
 
     def test_plain_report(self):
-        plain_report = self.r.build_report().replace("\n", "").replace(" ", "")
+        plain_report = self.r.build_report("html").replace("\n", "").replace(" ", "")
         self.assertEqual(plain_report, self.plain_report)
 
 
     def test_word_count_table(self):
-        wcr_table = self.wcr.build_table().replace("\n", "").replace(" ", "")
+        wcr_table = self.wcr.build_html_table().replace("\n", "").replace(" ", "")
         self.assertEqual(wcr_table, self.wcr_table)
 
 class ListingsRetrieverTestCase(unittest.TestCase):
